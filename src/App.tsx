4860653d823149062131b3b431cc20a6aa2facd5
default <mail@greenman999.de>
2025-05-10 15:35:48 +0000

    feat: add testing hero page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import type { Component } from "solid-js";
 
 const App: Component = () => {
-    return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <div class="hero bg-base-200 min-h-screen">
+            <div class="hero-content text-center">
+                <div class="max-w-md flex flex-col items-center">
+                    <h1 class="text-5xl font-bold">OddOneOut</h1>
+                    <p class="py-6">
+                        A fun party game where you have to find the odd one out!
+                    </p>
+                    <div class="flex gap-4 flex-row">
+                        <button class="btn btn-primary">Create game</button>
+                        <button class="btn btn-secondary">Join game</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
